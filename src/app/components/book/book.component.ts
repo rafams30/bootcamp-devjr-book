a64ff0cdc30e9497f1e82106bb12b6f5d28772bc
Rafael Martines <rafams_22@hotmail.com>
2024-06-28 21:35:53 -0300

    Crando objetos a partir de uma interface

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../../Book';
 
 @Component({
   selector: 'app-book',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class BookComponent {
 
+  book : Book = {
+    id : 1,
+    title : "Angular",
+    author : "Rafael",
+    price : 50.00
+  };
+
+  
 }
