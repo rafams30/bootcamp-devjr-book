24347e7369449e67fbbdfbd0289b57784409a2dc
Rafael Martines <rafams_22@hotmail.com>
2024-06-28 21:27:30 -0300

    Criando componentes - P2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
